d5fc871e8a33eae360cf11728b73b8362f0b45ea
Omri Doron <omridoron3@gmail.com>
2017-08-10 11:41:40 +0300

    first commit

---
+++ screens/OmriScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default class OmriScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Omris new screen</Text>
+     </View>
+    );
+  }
+}
